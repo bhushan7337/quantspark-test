64da69deec037bbc07ed5a2a02931620f13a8bce
bhushan7337 <bhushan7337@gmail.com>
2023-10-31 05:35:35 +0530

    Update var.tf

--- iac/var.tf
+++ iac/var.tf
@@ -7,11 +7,11 @@ variable "vpc_name" {
 variable "aws_key" {
   type        = string
   description = "key"
-  default     = "AKIA2PK5S6CRPGTHO5MM"
+  default     = ""
 }
 
 variable "aws_key_secret" {
   type        = string
   description = "key secret"
-  default     = "70u+66+H0tvr1CH3BI4JiXu0xMZoN5ZiImj/ckaP"
+  default     = ""
 }
